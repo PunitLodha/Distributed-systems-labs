bf66c848f6e18accbc134517695ca2646e414ca9
Phoenix09 <jaideepmore307@gmail.com>
2023-05-09 15:27:58 +0200

    lock server implementation

--- lock_server.cc
+++ lock_server.cc
@@ -5,10 +5,11 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <unordered_map>
 
-lock_server::lock_server():
-  nacquire (0)
+lock_server::lock_server() : nacquire(0)
 {
+  pthread_mutex_init(&lock_state_lock, NULL);
 }
 
 lock_protocol::status
@@ -20,4 +21,43 @@ lock_server::stat(int clt, lock_protocol::lockid_t lid, int &r)
   return ret;
 }
 
+lock_protocol::status
+lock_server::release(int clt, lock_protocol::lockid_t lid, int &r)
+{
+  pthread_mutex_lock(&lock_state_lock);
+  if (lock_state.count(lid) == 0)
+    return lock_protocol::RPCERR;
+  pthread_mutex_unlock(&lock_state_lock);
 
+  pthread_mutex_lock(&lock_mutex[lid]);
+  if (lock_state[lid] == false)
+    return lock_protocol::RPCERR;
+  lock_state[lid] = false;
+  pthread_mutex_unlock(&lock_mutex[lid]);
+  pthread_cond_signal(&lock_cv[lid]);
+}
+
+lock_protocol::status
+lock_server::acquire(int clt, lock_protocol::lockid_t lid, int &r)
+{
+  pthread_mutex_lock(&lock_state_lock);
+  if (lock_state.count(lid) == 0)
+  {
+    pthread_mutex_t mutex;
+    pthread_mutex_init(&mutex, NULL);
+    lock_mutex.insert({lid, mutex});
+
+    pthread_cond_t cv;
+    pthread_cond_init(&cv, NULL);
+    lock_cv.insert({lid, cv});
+
+    lock_state.insert({lid, false});
+  }
+  pthread_mutex_unlock(&lock_state_lock);
+
+  pthread_mutex_lock(&lock_mutex[lid]);
+  while (lock_state[lid] == true)
+    pthread_cond_wait(&lock_cv[lid], &lock_mutex[lid]);
+  lock_state[lid] = true;
+  pthread_mutex_unlock(&lock_mutex[lid]);
+}
